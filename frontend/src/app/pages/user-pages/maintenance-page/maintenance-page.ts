0a1af0fc40929f69b01a4963edda09cff11b25f1
tiago4vita <tiago.parejavita4@gmail.com>
2026-03-19 17:22:39 -0300

    feat: adiciona componente solicitation-row e suas variações

--- frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
+++ frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SolicitationFieldsHeaderComponent } from '../../../components/solicitation-fields-header/solicitation-fields-header';
+import { SolicitationRowComponent } from '../../../components/solicitation-row/solicitation-row';
 
 @Component({
   selector: 'app-maintenance-page',
-  imports: [SolicitationFieldsHeaderComponent],
+  imports: [SolicitationFieldsHeaderComponent, SolicitationRowComponent],
   templateUrl: './maintenance-page.html',
   styleUrl: './maintenance-page.scss'
 })
