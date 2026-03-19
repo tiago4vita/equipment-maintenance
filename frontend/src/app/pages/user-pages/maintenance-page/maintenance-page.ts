03a89a0e2c68dd0d74a715daeab59c8d0a9219cc
tiago4vita <tiago.parejavita4@gmail.com>
2026-03-19 17:00:27 -0300

    feat: adiciona header na tabela de solicitações do cliente

--- frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
+++ frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { SolicitationFieldsHeaderComponent } from '../../../components/solicitation-fields-header/solicitation-fields-header';
 
 @Component({
   selector: 'app-maintenance-page',
+  imports: [SolicitationFieldsHeaderComponent],
   templateUrl: './maintenance-page.html',
   styleUrl: './maintenance-page.scss'
 })
