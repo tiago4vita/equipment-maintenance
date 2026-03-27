4201af8ce2bbd185dddad51b70bc906b5a12df6b
tiago4vita <tiago.parejavita4@gmail.com>
2026-03-27 15:00:52 -0300

    feat: adiciona navbar

--- frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
+++ frontend/src/app/pages/user-pages/maintenance-page/maintenance-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ClienteNavbarComponent } from '../../../components/cliente-navbar/cliente-navbar';
 import { SolicitationFieldsHeaderComponent } from '../../../components/solicitation-fields-header/solicitation-fields-header';
 import { SolicitationRowComponent } from '../../../components/solicitation-row/solicitation-row';
 
 @Component({
   selector: 'app-maintenance-page',
-  imports: [SolicitationFieldsHeaderComponent, SolicitationRowComponent],
+  imports: [ClienteNavbarComponent, SolicitationFieldsHeaderComponent, SolicitationRowComponent],
   templateUrl: './maintenance-page.html'
 })
 export class MaintenancePageComponent {}
